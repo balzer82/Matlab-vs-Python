81d1b6e1f400ca8d9a4853b900e95aae56d59329
balzer82 <balzer82@gmail.com>
2015-03-23 10:01:59 +0100

    until 2015

--- compare.py
+++ compare.py
@@ -5,7 +5,7 @@
 
 import pandas as pd
 import matplotlib.pyplot as plt
-#%matplotlib inline
+%matplotlib inline
 
 # <markdowncell>
 
@@ -24,8 +24,9 @@ gd = data.pivot(index='month', columns='tag', values='count').fillna(0)
 gd.index = pd.to_datetime(gd.index)
 gd.columns.name = 'Frage zu'
 gd.rename(columns={'python': 'Python', 'matlab': 'Matlab', 'simulink': 'Simulink'}, inplace=True)
-gd.plot()
+gd[:'01-01-2014'].plot()
 plt.savefig('stockoverflow-gesamt.png')
+plt.savefig('stockoverflow-gesamt.eps')
 
 # <codecell>
 
@@ -33,9 +34,7 @@ gd['Matlab+Simulink'] = gd.Matlab+gd.Simulink
 
 # <codecell>
 
-for yearbegin in range(2010, 2013):
-    st = '01-01-%i' % yearbegin
-    df = gd[['Python','Matlab+Simulink']][st:'01-01-2014']
+df = gd[['Python','Matlab+Simulink']]['01-01-2009':'01-01-2014']
 
 # <headingcell level=4>
 
@@ -43,12 +42,12 @@ for yearbegin in range(2010, 2013):
 
 # <codecell>
 
-    def normalize(df):
-        return 100.0*df.values/df.values[0]
+def normalize(df):
+    return 100.0*df.values/df.values[0]
 
 # <codecell>
 
-    df = df.apply(normalize)
+df = df.apply(normalize)
 
 # <headingcell level=4>
 
@@ -56,10 +55,12 @@ for yearbegin in range(2010, 2013):
 
 # <codecell>
 
-    df.plot(title='Fragen auf stockoverflow.com nach Stichwort')
-    plt.ylabel='Prozent'
-    plt.ylim(0, 1000)
-    plt.savefig('stockoverflow-%i-2014.png' % yearbegin, dpi=72)
+df.plot()
+plt.ylabel='Prozent'
+plt.title='Fragen auf stockoverflow.com nach Stichwort'
+#plt.ylim(0, 1000)
+plt.savefig('stockoverflow-2009-2014.png', dpi=72)
+plt.savefig('stockoverflow-2009-2014.eps')
 
 # <codecell>
 
